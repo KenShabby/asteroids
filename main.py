3358294614c9bc2a9c933ad0ba9feb18e6d70a52
Brendan Coen <brendan@sonic.net>
2025-03-16 17:17:52 -0700

    First save point

---
+++ main.py
@@ -0,0 +1,24 @@
+from constants import *
+import pygame
+
+def main():
+
+    print(pygame.init())
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game Loop
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(255, 0, 0, 0)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
